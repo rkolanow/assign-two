1be7ea09971c25a89a09252a76385330c99d1b21
rkolanow <46732932+rkolanow@users.noreply.github.com>
2019-04-30 23:25:07 -0400

    Changing the code to pull 151 pokemon

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import styles from './App.module.css';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
+import Header from './components/Header/Header';
+import Profile from './pages/Profile/Profile';
+import Pokelist from './components/Pokelist/Pokelist';
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+        url: 'https://pokeapi.co/api/v2/pokemon',
+        list: [],
+        loading : true,
+        error: false,
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=20').then((response) => {
+  
+         this.setState({
+            list: response.data['results']
+        });
+  })}
+
+  render() {
+
+   
+    return (
+    <div className={styles.content}>
+      
+    <Header/>
+        <Route exact path='/:handle' component={Profile} />
+        <Route exact path ='/' component={Pokelist}/>
+    </div>
+       );
+        }        
+    }
+
+export default App;
+
